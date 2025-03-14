e1cc19ca1490f1f1eb735116e31b7067581e4557
elin-sunhye <tjsgp43@gmail.com>
2025-03-14 22:22:23 +0900

    header 및 공통 레이아웃 작업

---
+++ src/components/common/MainLeftlayout/style.js
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const layout = css`
+  box-sizing: border-box;
+  padding: 3rem;
+  width: 30rem;
+  height: 100%;
+  overflow: hidden;
+
+  background-color: #fff;
+`;
---
+++ src/routes/mainRoute/mainRoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../../components/common/Header/Header';
+import MainLeftlayout from '../../components/common/MainLeftlayout/MainLeftlayout';
+import SideMenuBox from '../../components/common/SIdeMenuBox/SIdeMenuBox';
+
+export default function MainRoute({}) {
+  return (
+    <>
+      <Header />
+
+      <MainLeftlayout>
+        {/* 유저 박스 여기 위치 */}
+        <SideMenuBox />
+      </MainLeftlayout>
+
+      {/* 푸터 여기 위치 */}
+    </>
+  );
+}
