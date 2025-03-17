08010edfc4ad46b43554f482cee95dc2d9b1c03a
Jeong-MinE <Jeong-MinE>
2025-03-17 11:21:35 +0900

    수정중

---
+++ src/components/Footer/style.js
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    width: auto;
+    height: 5rem;
+    background-color: #303740;
+`;
+
+export const job = css`
+    font-size: 0.6rem;
+    margin-left: 8rem;
+    margin-right: 2rem;
+    line-height: 0.9rem;
+    color: #ffffff;
+`;
+
+export const dreamPath = css`
+    font-size: 1.4rem;
+    letter-spacing: -0.1rem;
+    font-weight: bold;
+   
+    color: #ffffff;
+`;
+
+export const nameandemail = css`
+    font-size: 0.8rem;
+    margin-left: 2rem;
+    text-align: left;
+    line-height: 0.9rem;
+    color: #ffffff;
+`
+export const nameandemailLast = css`
+    font-size: 0.8rem;
+    margin-left: 2rem;
+    margin-right: 8rem;
+    text-align: left;
+    line-height: 0.9rem;
+    color: #ffffff;
+`
