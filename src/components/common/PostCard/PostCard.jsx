1d5ddb0fb83701f446594a8daa3de290fedb0ecf
elin-sunhye <tjsgp43@gmail.com>
2025-03-18 14:06:56 +0900

    mentoring 조회 작업중

--- src/components/common/PostCard/PostCard.jsx
+++ src/components/common/PostCard/PostCard.jsx
@@ -1,52 +1,52 @@
 /** @jsxImportSource @emotion/react */
+import moment from 'moment/moment';
 import * as s from './style';
 import React, { useEffect, useState } from 'react';
 import { FaHeart, FaStar } from 'react-icons/fa';
 
-export default function PostCard({
-    status,
-    viewCount,
-    title,
-    content,
-    nickname,
-    starPoint,
-    createdAt,
-}) {
-    const [isRecruiting, setIsRecruiting] = useState();
+const PostCard = React.forwardRef(
+    (
+        { status, viewCount, title, content, nickname, starPoint, createdAt },
+        ref
+    ) => {
+        const [isRecruiting, setIsRecruiting] = useState();
 
-    useEffect(() => {
-        setIsRecruiting(status === 'recruiting' ? true : false);
-    }, [status]);
+        useEffect(() => {
+            setIsRecruiting(status === 'recruiting' ? true : false);
+        }, [status]);
 
-    return (
-        <div css={s.cardBox}>
-            <div css={s.top}>
-                <span css={s.chip(isRecruiting)}>
-                    {status === 'recruiting' ? '모집중' : '모집마감'}
-                </span>
-                <div css={s.heartBox}>
-                    <FaHeart />
-                    <span>{viewCount}</span>
-                </div>
-            </div>
-            <div css={s.titleBox}>
-                <p>{title}</p>
-                <p>{content}</p>
-            </div>
-
-            <div css={s.infoBox}>
-                <div>
-                    <p>{nickname}</p>
-                    <div css={s.starBox}>
-                        {Array.from({ length: starPoint }, (index) => (
-                            <p key={`starPont_${index}`}>
-                                <FaStar />
-                            </p>
-                        ))}
+        return (
+            <div ref={ref} css={s.cardBox}>
+                <div css={s.top}>
+                    <span css={s.chip(isRecruiting)}>
+                        {status === 'recruiting' ? '모집중' : '모집마감'}
+                    </span>
+                    <div css={s.heartBox}>
+                        <FaHeart />
+                        <span>{viewCount}</span>
                     </div>
                 </div>
-                <p>{createdAt}</p>
+                <div css={s.titleBox}>
+                    <p>{title}</p>
+                    <p>{content}</p>
+                </div>
+
+                <div css={s.infoBox}>
+                    <div>
+                        <p>{nickname}</p>
+                        <div css={s.starBox}>
+                            {Array.from({ length: starPoint }, (index) => (
+                                <p key={`starPont_${index}`}>
+                                    <FaStar />
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                    <p>{moment(createdAt).format('YYYY-MM-DD')}</p>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
+);
+
+export default PostCard;
