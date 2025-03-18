69d05b7844c056fc01a27ad86a81257f37b042b6
MunIL HONG <bear4564@naver.com>
2025-03-18 13:56:32 +0900

    ㅇㅋ시간라이브러리한번써볼게^^

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -3,3 +3,6 @@ import { api } from '../configs/axiosConfig';
 export const postsApi = async (boardName, params) => {
     return await api.get(`/api/posts/${boardName}`, { params });
 };
+
+export const updateNoticePostApi = async (notice) => await api.post(`/api/notice/${notice.categoryName}`, {title: notice.title, content: notice.content});
+
