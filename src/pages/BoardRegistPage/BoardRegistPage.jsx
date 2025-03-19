a499e72392b7bbb69f6c35706a6cbc2922b46955
elin-sunhye <tjsgp43@gmail.com>
2025-03-19 11:37:11 +0900

    게시글 등록 작업중

--- src/pages/BoardRegistPage/BoardRegistPage.jsx
+++ src/pages/BoardRegistPage/BoardRegistPage.jsx
@@ -5,6 +5,11 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useGetBoards } from '../../queries/boardQuery';
+import Select from 'react-select';
+import { useGetMentoringCategories } from '../../queries/mentoringQuery';
+import DaumPostcode from 'react-daum-postcode';
+import { IoClose } from 'react-icons/io5';
+import { DatePicker } from '@mui/x-date-pickers';
 
 export default function BoardRegistPage({}) {
     const navigation = useNavigate();
@@ -17,7 +22,6 @@ export default function BoardRegistPage({}) {
 
     useEffect(() => {
         if (boardList && boardList.data && boardList.data.data) {
-            console.log(pathNm);
             let newArray = boardList.data.data.filter(
                 (board) => board.boardName == pathNm.boardName
             )[0];
@@ -25,32 +29,17 @@ export default function BoardRegistPage({}) {
         }
     }, [boardList.data]);
 
-    useEffect(() => {
-        // 권한 체크 및 페이지 체크
-        if (!!localStorage.getItem('AccessToken')) {
-            if (board.boardId > 3) {
-                // 로그인 되어 있지만 등록 페이지가 없는 게시판일 경우 무조건 메인으로
-                navigation('/');
-            }
-        } else {
-            // 로그인 전이면 무조건 메인으로
-            navigation('/');
-        }
-    }, [board]);
-
     // quill
     const contaiinerQuillRef = useRef();
 
     const [quill, setQuill] = useState(null);
-    const [quillTitle, setQuillTitle] = useState('');
-    const [quillContent, setQuillContent] = useState('');
 
     useEffect(() => {
         const toolbarOptions = [
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
             [{ font: [] }],
-            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ color: [] }, { background: [] }],
             [{ align: [] }],
             ['link', 'image', 'video', 'formula'],
         ];
@@ -66,9 +55,80 @@ export default function BoardRegistPage({}) {
         setQuill(quill);
 
         quill.on('text-change', () => {
-            setQuillContent(quill.root.innerHTML);
+            setRegistData((prev) => ({
+                ...prev,
+                content: quill.root.innerHTML,
+            }));
         });
     }, []);
+
+    // 카테고리 리스트 데이터
+    const categories = useGetMentoringCategories();
+    const [categoriesSelectOption, setCategoriesSelectOption] = useState();
+
+    useEffect(() => {
+        if (categories && categories.data && categories.data.data) {
+            const newArray = [];
+            categories.data.data.map((category) =>
+                newArray.push({
+                    value: category.mentoringCategoryId,
+                    label: category.mentoringCategoryName,
+                })
+            );
+
+            setCategoriesSelectOption(newArray);
+        }
+    }, [categories.data]);
+
+    // 주소 찾기 모달
+    const [findAddressModalOpen, setFindAddressModalOpen] = useState();
+    function handleFindAddressOnClick() {
+        setFindAddressModalOpen(!findAddressModalOpen);
+    }
+
+    // 게시글 등록
+    const [registData, setRegistData] = useState({
+        boardId: board.boardId,
+        mentoringCategoryId: 0,
+        title: '',
+        content: '',
+        mentoringAddress: '',
+        mentoringAddressDetail: '',
+        startDate: '',
+        endDate: '',
+        status: '',
+        attachedFiles: '',
+    });
+
+    useEffect(() => {
+        console.log('registData', registData);
+    }, [registData]);
+
+    useEffect(() => {
+        // 권한 체크 및 페이지 체크
+        if (!!localStorage.getItem('AccessToken')) {
+            if (board.boardId > 3) {
+                // 로그인 되어 있지만 등록 페이지가 없는 게시판일 경우 무조건 메인으로
+                navigation('/');
+            }
+        } else {
+            // 로그인 전이면 무조건 메인으로
+            navigation('/');
+        }
+
+        // 등록 보드 아이디
+        setRegistData((prev) => ({
+            ...prev,
+            boardId: board.boardId,
+        }));
+    }, [board]);
+
+    function handleInpOnChange(e) {
+        setRegistData((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    }
 
     return (
         <>
@@ -87,6 +147,8 @@ export default function BoardRegistPage({}) {
                                 ? '제목에 자기 어필 내용을 요약해보세요.'
                                 : '제목을 입력하세요.'
                         }
+                        value={registData.title}
+                        onChange={handleInpOnChange}
                     />
                 </div>
                 <div css={s.qullBox}>
@@ -95,25 +157,78 @@ export default function BoardRegistPage({}) {
 
                 {board.boardId == 1 ? (
                     <>
-                        <div>
+                        <div css={s.selectBox} className="selectBox">
                             <label htmlFor="category">카테고리</label>
-                            <input type="text" name="category" id="category" />
+                            <Select
+                                options={categoriesSelectOption}
+                                name="mentoringCategoryId"
+                                styles={{
+                                    control: (style) => ({
+                                        ...style,
+                                        boxSizing: 'border-box',
+                                        borderRadius: '1rem',
+                                        borderColor: '#aaa',
+                                        width: '100%',
+                                        height: '4rem',
+                                        minHeight: 'unset',
+                                        fontSize: '1.3rem',
+                                    }),
+                                    dropdownIndicator: (style) => ({
+                                        ...style,
+                                        padding: '0.3rem',
+                                    }),
+                                }}
+                                value={categoriesSelectOption.find(
+                                    (option) =>
+                                        option.value ===
+                                        registData.mentoringCategoryId
+                                )}
+                                onChange={(option) => {
+                                    setRegistData((prev) => ({
+                                        ...prev,
+                                        mentoringCategoryId: option.value,
+                                    }));
+                                }}
+                            />
                         </div>
                         <div>
                             <label htmlFor="">멘토링 일자</label>
                             <div css={s.dtBox}>
-                                <input
-                                    type="text"
-                                    name="startDt"
-                                    id="startDt"
+                                <DatePicker
+                                    name="startDate"
+                                    id="startDate"
+                                    // value={registData.startDate}
+                                    // onChange={handleInpOnChange}
                                 />
-                                <input type="text" name="endDt" id="endDt" />
+                                <DatePicker
+                                    name="endDate"
+                                    id="endDate"
+                                    // value={registData.endDate}
+                                    // onChange={handleInpOnChange}
+                                />
                             </div>
                         </div>
-                        <div>
+                        <div css={s.addressBox}>
                             <label htmlFor="address">주소</label>
-                            <input type="text" name="address" id="address" />
-                            <input type="text" name="address" id="address" />
+                            <input
+                                type="text"
+                                name="mentoringAddress"
+                                id="mentoringAddress"
+                                value={registData.mentoringAddress}
+                                disabled
+                            />
+                            <button
+                                type="button"
+                                onClick={handleFindAddressOnClick}
+                            >
+                                주소 찾기
+                            </button>
+                            <input
+                                type="text"
+                                name="mentoringAddressDetail"
+                                id="mentoringAddressDetail"
+                                onChange={handleInpOnChange}
+                            />
                         </div>
                         <div>
                             <label htmlFor="attachedFile">첨부파일</label>
@@ -127,6 +242,37 @@ export default function BoardRegistPage({}) {
                 ) : (
                     <input type="file" name="" id="" />
                 )}
+            </div>
+
+            {/* 주소 찾기 모달창 */}
+            <div css={s.findAddressModalBox(findAddressModalOpen)}>
+                <div>
+                    <p>
+                        주소 찾기
+                        <button
+                            type="button"
+                            onClick={handleFindAddressOnClick}
+                        >
+                            <IoClose />
+                        </button>
+                    </p>
+                    <DaumPostcode
+                        style={{
+                            width: '50rem',
+                            height: '45rem',
+                        }}
+                        onComplete={(data) => {
+                            const detailAddress = '';
+
+                            setFindAddressModalOpen(false);
+
+                            setRegistData((prev) => ({
+                                ...prev,
+                                mentoringAddress: data.address,
+                            }));
+                        }}
+                    />
+                </div>
             </div>
         </>
     );
--- src/pages/BoardRegistPage/style.js
+++ src/pages/BoardRegistPage/style.js
@@ -31,15 +31,12 @@ export const contentBox = css`
         width: 100%;
 
         &:not(:first-of-type) {
-            label,
-            div {
-                width: 100%;
-            }
-
             label {
                 display: inline-block;
 
                 margin-bottom: 1rem;
+
+                width: 100%;
                 font-size: 1.6rem;
                 font-weight: bold;
 
@@ -49,6 +46,13 @@ export const contentBox = css`
                     margin-right: 0.5rem;
                 }
             }
+        }
+
+        &:not(:first-of-type, .selectBox) {
+            div {
+                width: 100%;
+            }
+
             input {
                 box-sizing: border-box;
                 padding: 0 1rem;
@@ -114,6 +118,8 @@ export const qullBox = css`
     }
 `;
 
+export const selectBox = css``;
+
 export const dtBox = css`
     display: flex;
     justify-content: space-between;
@@ -121,4 +127,80 @@ export const dtBox = css`
     gap: 1rem;
 
     width: 100%;
+`;
+
+export const addressBox = css`
+    input {
+        &:first-of-type {
+            margin-right: 1rem;
+            margin-bottom: 0.5rem;
+
+            width: calc(100% - 9rem) !important;
+        }
+    }
+
+    button {
+        box-sizing: border-box;
+        padding: 1.2rem 0;
+        outline: none;
+        border: none;
+        border-radius: 1rem;
+
+        width: 8rem;
+
+        color: #fff;
+
+        background-color: #1681ff;
+        cursor: pointer;
+    }
+`;
+
+export const findAddressModalBox = (findAddressModalOpen) => css`
+    display: ${findAddressModalOpen ? 'flex' : 'none'};
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+
+    background-color: #3333335b;
+
+    & > div {
+        box-sizing: border-box;
+        padding: 2rem 5rem;
+        border-radius: 2rem;
+
+        width: fit-content;
+        background-color: #fff;
+
+        p {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            font-size: 2rem;
+            font-weight: bold;
+
+            button {
+                box-sizing: border-box;
+                padding: 0;
+                outline: none;
+                border: none;
+
+                width: 3rem;
+                height: 3rem;
+
+                background-color: #fff;
+                cursor: pointer;
+
+                svg {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+    }
 `;
