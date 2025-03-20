95c62388878013fabd7d4b6631b631e5b86c5f92
ksu14044 <mhm1404@naver.com>
2025-03-20 11:54:10 +0900

    티켓구매내역 페이지네이션

--- src/pages/TicketPurchasePage/TicketPurchasePage.jsx
+++ src/pages/TicketPurchasePage/TicketPurchasePage.jsx
@@ -2,9 +2,9 @@
 import { useSearchParams } from 'react-router-dom';
 import * as s from './style';
 import React, { useEffect, useState } from 'react';
-import { useGetSearchPointPurchaseList } from '../../queries/pointQuery';
 import Select from 'react-select';
 import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
+import { useGetSearchTicketPurchaseList } from '../../queries/ticketQuery';
 
 function TicketPurchasePage(props) {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -16,7 +16,7 @@ function TicketPurchasePage(props) {
         { value: "asc", label: "오래된순" },
     ];
 
-    const searchPointPurchaseList = useGetSearchPointPurchaseList({
+    const searchTicketPurchaseList = useGetSearchTicketPurchaseList({
         page,
         limitCount: 10,
         order,
@@ -25,9 +25,9 @@ function TicketPurchasePage(props) {
     const [pageNumbers, setPageNumbers] = useState([]);
 
     useEffect(() => {
-        if (!searchPointPurchaseList.isLoading) {
-            const currentPage = searchPointPurchaseList?.data?.data.page || 1;
-            const totalPages = searchPointPurchaseList?.data?.data.totalPages || 1;
+        if (!searchTicketPurchaseList.isLoading) {
+            const currentPage = searchTicketPurchaseList?.data?.data.page || 1;
+            const totalPages = searchTicketPurchaseList?.data?.data.totalPages || 1;
             const startIndex = Math.floor((currentPage - 1) / 5) * 5 + 1;
             const endIndex = startIndex + 4 > totalPages ? totalPages : startIndex + 4;
 
@@ -38,11 +38,11 @@ function TicketPurchasePage(props) {
             setPageNumbers(newPageNumbers);
 
         }
-    }, [searchPointPurchaseList.data]);
+    }, [searchTicketPurchaseList.data]);
 
     useEffect(() => {
-        console.log(searchPointPurchaseList)
-        searchPointPurchaseList.refetch();
+        searchTicketPurchaseList.refetch();
+        console.log(searchTicketPurchaseList);
     }, [searchParams])
 
     const handleSelectOnChange = (option) => {
@@ -59,7 +59,7 @@ function TicketPurchasePage(props) {
     return (
         <div css={s.container}>
             <div css={s.titleSelectBar}>
-                <h2 css={s.title}>포인트 충전 내역</h2>
+                <h2 css={s.title}>티켓 구매 내역</h2>
                 <Select
                     options={orderSelectOptions}
                     styles={{
@@ -86,18 +86,18 @@ function TicketPurchasePage(props) {
                 <table css={s.table}>
                     <thead>
                         <tr css={s.tableRowHeader}>
+                            <th css={s.tableHeader}>이용권</th>
                             <th css={s.tableHeader}>포인트</th>
-                            <th css={s.tableHeader}>결제금액</th>
                             <th css={s.tableHeader}>구매일자</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            searchPointPurchaseList?.data?.data.pointPurchaseSearchList.map((point, index) => (
+                        {  
+                            searchTicketPurchaseList?.data?.data.ticketPurchaseHistoryList.map((ticket, index) => (
                                 <tr key={index} css={s.tableRow}>
-                                    <td css={s.tableCell}>{point.pointName}</td>
-                                    <td css={s.tableCell}>{Number(point.pointPrice).toLocaleString()}원</td>
-                                    <td css={s.tableCell}>{point.createdAt}</td>                      
+                                    <td css={s.tableCell}>{ticket.ticketName}</td>
+                                    <td css={s.tableCell}>{Number(ticket.price).toLocaleString()}P</td>
+                                    <td css={s.tableCell}>{ticket.createdAt}</td>                      
                                 </tr>
                             ))
                         }
@@ -106,13 +106,13 @@ function TicketPurchasePage(props) {
             </div>
             <div css={s.footer}>
                 <div css={s.pageNumbers}>
-                    <button disabled={searchPointPurchaseList?.data?.data.firstPage} onClick={() => handlePageNumbersOnClick(page - 1)}><GoChevronLeft /></button>
+                    <button disabled={searchTicketPurchaseList?.data?.data.firstPage} onClick={() => handlePageNumbersOnClick(page - 1)}><GoChevronLeft /></button>
                     {
                         pageNumbers.map(number => 
-                            <button key={number} css={s.pageNum(page === number)} onClick={() => handlePageNumbersOnClick(number)}><span>{number}</span></button>
+                            <button key={`ticket${number}`} css={s.pageNum(page === number)} onClick={() => handlePageNumbersOnClick(number)}><span>{number}</span></button>
                         )
                     }
-                    <button disabled={searchPointPurchaseList?.data?.data.lastPage} onClick={() => handlePageNumbersOnClick(page + 1)}><GoChevronRight /></button>
+                    <button disabled={searchTicketPurchaseList?.data?.data.lastPage} onClick={() => handlePageNumbersOnClick(page + 1)}><GoChevronRight /></button>
                 </div>
             </div>
         </div>
