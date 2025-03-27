ed4c85c48b7fbc5a007ea70f17628d80f8bba902
ksu14044 <mhm1404@naver.com>
2025-03-27 11:34:01 +0900

    홈페이지 수정

--- src/components/common/TopRightLayout/style.js
+++ src/components/common/TopRightLayout/style.js
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 export const layout = css`
   box-sizing: border-box;
   padding: 3rem;
-  width: calc(100% - 35rem);
+  width: calc(100% - 30rem);
   height: auto;
   overflow: hidden;
 
