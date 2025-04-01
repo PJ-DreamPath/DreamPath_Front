7b7679b0a40dfa686d771cce5f5dce8b0f5c4f5b
elin-sunhye <tjsgp43@gmail.com>
2025-04-01 09:05:31 +0900

    main .map() key 오류 수정

--- src/components/HomePostList/HomePostList.jsx
+++ src/components/HomePostList/HomePostList.jsx
@@ -10,40 +10,40 @@ function HomePostList(props) {
     const navigation = useNavigate();
 
     const [search, setSearch] = useState({
-                page: 1,
-                limitCount: 4,
-                order: 'starDesc',
-                searchTxt: '',
-            });
+        page: 1,
+        limitCount: 4,
+        order: 'starDesc',
+        searchTxt: '',
+    });
 
     const mentoringPostList = useGetPosts(1, search);
 
     useEffect(() => {
-        if (mentoringPostList && mentoringPostList.data && mentoringPostList.data.data) {
-        console.log('mentoringPostList', mentoringPostList);
+        if (
+            mentoringPostList &&
+            mentoringPostList.data &&
+            mentoringPostList.data.data
+        ) {
+            console.log('mentoringPostList', mentoringPostList);
         }
-    }, [mentoringPostList?.data])
+    }, [mentoringPostList?.data]);
     return (
         <div css={s.postListContainer}>
-            {
-                mentoringPostList?.data?.data.postList.map((post) => 
-                    <HomePostCard
-                        key={`homePost_${post.postId}`}
-                        status={post.status}
-                        likeCount={post.likeCount}
-                        title={post.title}
-                        content={post.content}
-                        nickname={post.user.nickname}
-                        starPoint={post.starPoint}
-                        createdAt={post.createdAt}
-                        onClick={() => {
-                            navigation(
-                                `/service/mentoring/${post.postId}`
-                            );
-                        }}
-                    />
-                )
-            }
+            {mentoringPostList?.data?.data.postList.map((post) => (
+                <HomePostCard
+                    key={`mentoring_main_${post.postId}`}
+                    status={post.status}
+                    likeCount={post.likeCount}
+                    title={post.title}
+                    content={post.content}
+                    nickname={post.user.nickname}
+                    starPoint={post.starPoint}
+                    createdAt={post.createdAt}
+                    onClick={() => {
+                        navigation(`/service/mentoring/${post.postId}`);
+                    }}
+                />
+            ))}
         </div>
     );
 }
